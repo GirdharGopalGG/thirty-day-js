421f8f6c9be08af1b568459fd0350a6c47066693
Girdhar <greatgmang@gmail.com>
2024-10-01 09:42:22 +0530

    half of Day 1

---
+++ Day 1-Variables and Data Types/1.js
@@ -0,0 +1,23 @@
+var num = 2;
+let name = "git";
+const istrue = false;
+
+console.log(num);
+console.log(name);
+console.log(istrue);
+
+let n1 = 20;
+let address = "road no 10";
+let isDead = false;
+let about = {
+  name: "mukesh",
+  age: 30,
+  height: 5.5,
+  weight: "Lightweight",
+};
+let array = [1, 2, 3, 4, 5];
+console.log(typeof n1);
+console.log(typeof address);
+console.log(typeof isDead);
+console.log(typeof about);
+console.log(typeof array);
